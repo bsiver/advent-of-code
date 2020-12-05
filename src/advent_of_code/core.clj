91f5dfa3ccf457e98883ceb9c0a8f6d303c16bdb
Ben Siver <ben.siver@sproutsocial.com>
2020-12-04 20:05:06 -0600

    Added problem 1 solution

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,7 +1,19 @@
 (ns advent-of-code.core
   (:gen-class))
 
+(defn read-problem-input
+  [file-name]
+  (with-open [rdr (clojure.java.io/reader file-name)]
+    (reduce conj [] (line-seq rdr))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  (let [in (map #(Integer/parseInt %) (read-problem-input "resources/p1-input.txt"))
+        target-sum 2020]
+    (doseq [x in]
+      (if (some #{(- target-sum x)} in)
+        (let [match (- target-sum x)]
+          (println (format "Found a match! %s %s" x match))
+          (println (format "Result is %s" (* x match))))))))
