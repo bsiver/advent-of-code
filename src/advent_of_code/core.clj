4f4275b6f3998bca63488dcf6e0576663fd4654b
Ben Siver <ben.siver@sproutsocial.com>
2020-12-05 09:23:53 -0600

    Added day 2 solution

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,19 +1,39 @@
 (ns advent-of-code.core
-  (:gen-class))
+  (:require [clojure.string :as str]))
 
 (defn read-problem-input
   [file-name]
   (with-open [rdr (clojure.java.io/reader file-name)]
     (reduce conj [] (line-seq rdr))))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
 
-  (let [in (map #(Integer/parseInt %) (read-problem-input "resources/p1-input.txt"))
-        target-sum 2020]
-    (doseq [x in]
-      (if (some #{(- target-sum x)} in)
+(defn p1
+  [input]
+  (let [target-sum 2020]
+    (doseq [x input]
+      (if (some #{(- target-sum x)} input)
         (let [match (- target-sum x)]
           (println (format "Found a match! %s %s" x match))
           (println (format "Result is %s" (* x match))))))))
+
+
+(defn p2-is-valid
+  [line]
+  (let [components (str/split line #" ")
+        range (str/split (first components) #"-")
+        range-min (Integer/parseInt (first range))
+        range-max (Integer/parseInt (second range))
+        rule-char (first (second components))
+        password (nth components 2)
+        occurrence-count (get (frequencies password) rule-char 0)]
+        (<= range-min occurrence-count range-max)))
+
+(defn p2
+  [input]
+  (count (filter p2-is-valid input)))
+
+
+(defn -main
+  [& args]
+  ;(p1 (read-problem-input ("resources/p1-input.txt")))
+  (println (format "P2 answer: %s" (p2 (read-problem-input "resources/p2-input.txt")))))
