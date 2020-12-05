53d7bea637af74a5d364f0a6c6e5ad506050ef63
Ben Siver <ben.siver@sproutsocial.com>
2020-12-05 10:44:03 -0600

    Added p2 part 2 solution

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -28,12 +28,32 @@
         occurrence-count (get (frequencies password) rule-char 0)]
         (<= range-min occurrence-count range-max)))
 
+(defn p2-part2-is-valid
+  [line]
+  (let [components (str/split line #" ")
+        indices (str/split (first components) #"-")
+        index-one  (dec (Integer/parseInt (first indices)))
+        index-two (dec (Integer/parseInt (second indices)))
+        rule-char (first (second components))
+        password (nth components 2)
+        first-char-to-check (nth password index-one 0)
+        second-char-to-check (nth password index-two 0)]
+    (do
+      (println (format "Checking pw %s | indices %s | i1 %s i2 %s | first-char %s | second-char %s | rule-char %s"
+               password indices index-one index-two first-char-to-check second-char-to-check rule-char)))
+    (Boolean/logicalXor (= first-char-to-check rule-char) (= second-char-to-check rule-char))))
+
 (defn p2
   [input]
   (count (filter p2-is-valid input)))
+
+(defn p2-part2
+  [input]
+  (count (filter p2-part2-is-valid input)))
 
 
 (defn -main
   [& args]
   ;(p1 (read-problem-input ("resources/p1-input.txt")))
-  (println (format "P2 answer: %s" (p2 (read-problem-input "resources/p2-input.txt")))))
+  ;(println (format "P2 answer: %s" (p2 (read-problem-input "resources/p2-input.txt"))))
+  (println (format "P2-2 answer: %s" (p2-part2 (read-problem-input "resources/p2-input.txt")))))
