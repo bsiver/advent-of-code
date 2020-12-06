670ce792d850d038634043119e3ec2866ebdb6d3
Ben Siver <ben.siver@sproutsocial.com>
2020-12-05 21:39:06 -0600

    Added day part 2 solution

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -18,8 +18,8 @@
 
 
 (defn -main
-  [& args]
+  [& args])
   ;(p1 (read-problem-input ("resources/p1-input.txt")))
   ;(println (format "P2 answer: %s" (p2 (read-problem-input "resources/p2-input.txt"))))
   ;(println (format "P2-2 answer: %s" (p2-part2 (read-problem-input "resources/p2-input.txt")))))
-  (println (format "P3 answer: %s" (p3 (read-problem-input "resources/p3-input.txt")))))
+  ;(println (format "P3 answer: %s" (p3 (read-problem-input "resources/p3-input.txt")))))
--- src/advent_of_code/day_1.clj
+++ src/advent_of_code/day_1.clj
@@ -3,13 +3,27 @@
 
 (defn p1
   [input]
-  (let [target-sum 2020]
+  (let [input (map #(Integer/parseInt %) input)
+        target-sum 2020]
     (doseq [x input]
       (if (some #{(- target-sum x)} input)
         (let [match (- target-sum x)]
           (println (format "Found a match! %s %s" x match))
           (* x match))))))
 
+
+(defn tails [data]
+  (take-while seq (iterate rest data)))
+
+(defn p1-part2
+  [input]
+  (let [in (map #(Integer/parseInt %) input)]
+    (for [[x & xs] (tails in)
+          [y & ys] (tails xs)
+          [z] (tails ys)
+          :when (= 2020 (+ x y z))]
+      (reduce * [x y z]))))
+
 (defn -main
   [& args]
-  (println (p1 (util/read-problem-input ("resources/p1-input.txt")))))
+  (println (p1-part2 (util/read-problem-input "resources/p1-input.txt"))))
