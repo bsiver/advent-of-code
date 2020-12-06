96477425815eef6c52cfe6c01cde8830202a5162
Ben Siver <ben.siver@sproutsocial.com>
2020-12-05 22:56:18 -0600

    Added day 3 part 2 solution

--- src/advent_of_code/day_3.clj
+++ src/advent_of_code/day_3.clj
@@ -18,9 +18,31 @@
           (println (format "Tree hits: %s" tree-hits))
 
           (recur (+ 3 res) (rest input) (if hit-tree (inc tree-hits) tree-hits))))
-
       res)))
+
+(defn p3-part2
+  [input x-dir y-dir]
+  (loop [res 0
+         input input
+         tree-hits 0]
+    (if (seq input)
+      (do
+        (let [this-row (first input)
+              index (mod res (count this-row))
+              this-char (nth this-row index)
+              hit-tree (= this-char \#)
+              hits (if hit-tree (inc tree-hits) tree-hits)]
+          ;(println (format "res: %s %s " index this-row))
+          ;(println (format "Character at this index %s" this-char))
+          ;(println (format "Hit tree? %s" hit-tree))
+          (recur (+ x-dir res) (drop y-dir input) hits)))
+      tree-hits)))
 
 (defn -main
   [& args]
-  (println (p3 (util/read-problem-input ("resources/p3-input.txt")))))
+  (println (p3-part2 (util/read-problem-input "resources/p3-input.txt") 1 1))
+  (println (p3-part2 (util/read-problem-input "resources/p3-input.txt") 3 1))
+  (println (p3-part2 (util/read-problem-input "resources/p3-input.txt") 5 1))
+  (println (p3-part2 (util/read-problem-input "resources/p3-input.txt") 7 1))
+  (println (p3-part2 (util/read-problem-input "resources/p3-input.txt") 1 2))
+  )
