891491f1b7944aad9b26504f253700797090765d
Ben Siver <ben.siver@sproutsocial.com>
2020-12-09 19:41:29 -0600

    Added day 8 part 2 solution

--- src/advent_of_code/day_8.clj
+++ src/advent_of_code/day_8.clj
@@ -24,10 +24,41 @@
                   "acc" (+ x acc)
                   "jmp" acc
                   "nop" acc)]
-        (println (format "cur-ins %s | x %s | next-i %s | new-acc %s" cur-instruction x next-i new-acc))
           (recur next-i (conj seen-instructions i) new-acc))
       acc)))
 
+(defn read-terminates?
+  [imap]
+  (loop [i 0
+         seen-instructions #{}
+         acc 0]
+    (cond (= i (count imap)) acc
+          (not (contains? seen-instructions i))
+          (let [cur-instruction (get-in imap [i :instr])
+                x (Integer/parseInt (get-in imap [i :x]))
+                next-i (condp = cur-instruction
+                         "acc" (inc i)
+                         "jmp" (+ i x)
+                         "nop" (inc i))
+                new-acc (condp = cur-instruction
+                          "acc" (+ x acc)
+                          "jmp" acc
+                          "nop" acc)]
+            (recur next-i (conj seen-instructions i) new-acc))
+          :else nil)))
+
+(defn permute-instructions
+  [imap]
+  (let [instrs  imap
+        end   (count imap)
+        instr-complement {"acc" "acc", "nop" "jmp", "jmp" "nop"}]
+    (loop [i 0, variants ()]
+      (cond
+        (= i end) (reverse variants)
+        :else
+        (recur (inc i) (cons (assoc instrs i {:instr (instr-complement (get-in instrs [i :instr]))
+                                            :x (get-in instrs [i :x])})
+                               variants))))))
 
 
 (defn p1
@@ -38,7 +69,8 @@
 
 (defn p2
   [input]
-  )
+  (let [permuted-instructions (permute-instructions (reduce parse-line {} (map-indexed vector input)))]
+    (filter some? (map read-terminates? permuted-instructions))))
 
 (defn -main
   [& args])
