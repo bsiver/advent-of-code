f452bad714d7e062538e44d688771b6f9bc6e53a
Ben Siver <bsiver@gmail.com>
2021-12-02 09:06:05 -0600

    Added day 2 part 2 solution

--- 2021/p2.py
+++ 2021/p2.py
@@ -13,3 +13,19 @@ for k, v in split_lines:
         y -= v
 
 print(f'Day 2 ({x}, {y}): {x*y}')
+
+
+x = 0
+y = 0
+aim = 0
+for k, v in split_lines:
+    v = int(v)
+    if k == 'forward':
+        x += v
+        y += aim * v
+    elif k == 'down':
+        aim += v
+    elif k == 'up':
+        aim -= v
+
+print(f'Day 2 ({x}, {y}): {x * y}')
