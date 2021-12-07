810db70e7d0d23edb43eeb67846a6467c394c6e8
Ben Siver <bsiver@gmail.com>
2021-12-07 08:45:00 -0600

    Day 6 part 2

--- 2021/p6.py
+++ 2021/p6.py
@@ -1,9 +1,10 @@
+from collections import Counter
+
 from util import get_file_contents
 
-fish = [int(x) for x in get_file_contents('p6-ex.txt')[0].split(',')]
+fish = [int(x) for x in get_file_contents('p6.txt')[0].split(',')]
 
 print(f'Initial state: {fish}')
-
 
 def part_one(n):
     for i in range(1, n+1):
@@ -18,9 +19,22 @@ def part_one(n):
                 fish[j] = new_val
     return fish
 
+def part_two(n):
+    f = Counter(fish)
+    for i in range(n):
+        new_fish_count = f[0]
+        # increment all ages
+        for i in range(8):
+            f[i] = f[i + 1]
+        f[8] = new_fish_count
+        f[6] += new_fish_count
+    return f
 
 
-print(f'After 80 days len: {len(part_one(80))}')
+
+#print(f'After 80 days len: {len(part_one(80))}')
+
+print(f'After 80 days len: {sum(part_two(256).values())}')
 
 
 
